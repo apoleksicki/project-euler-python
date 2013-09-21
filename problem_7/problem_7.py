182cb3fa3157cd4945ed1030652ece47f748bfc6
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-09-21 21:38:11 +0200

    Created module for problem 7.

---
+++ problem_7/problem_7.py
@@ -0,0 +1,17 @@
+'''
+Created on 21/09/2013
+
+10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+@author: Antek
+'''
+
+def prime_numbers(n):
+    pass
+
+if __name__ == '__main__':
+    pass
